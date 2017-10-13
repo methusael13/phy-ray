5d438bf6f398584dd5f4a9d17cb6bcda3a9e6221
Methusael Murmu <blendit07@gmail.com>
2017-10-13 13:13:09 +0530

    Placeholder for materials

---
+++ phyray_lib/include/core/material/material.h
@@ -0,0 +1,13 @@
+#ifndef PHYRAY_CORE_MATERIAL_H
+#define PHYRAY_CORE_MATERIAL_H
+
+#include <core/phyr.h>
+
+namespace phyr {
+
+// Placeholder for Material related classes
+class TransportMode {};
+
+}
+
+#endif
