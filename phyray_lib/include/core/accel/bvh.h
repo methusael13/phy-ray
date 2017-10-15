93f9966fe1ae5186438c5c78942561e46c95e6e9
Methusael Murmu <blendit07@gmail.com>
2017-10-15 10:27:15 +0530

    Created BVH foundation

--- phyray_lib/include/core/accel/bvh.h
+++ phyray_lib/include/core/accel/bvh.h
@@ -9,16 +9,28 @@
 
 namespace phyr {
 
-typedef struct _BHVObjectInfo BHVObjectInfo;
+typedef struct _BVHTreeNode BVHTreeNode;
+typedef struct _BVHObjectInfo BVHObjectInfo;
 
-struct _BHVObjectInfo {
-    _BHVObjectInfo(size_t objectIdx, const Bounds3f& bounds) :
+struct _BVHObjectInfo {
+    _BVHObjectInfo(size_t objectIdx, const Bounds3f& bounds) :
         objectIdx(objectIdx), bounds(bounds),
         centroid(0.5 * bounds.pMin + 0.5 * bounds.pMax) {}
 
-    size_t objectIdx;
-    Bounds3f bounds;
+    size_t objectIdx; Bounds3f bounds;
     Point3f centroid;
+};
+
+struct _BVHTreeNode {
+    Bounds3f bounds;
+    BVHTreeNode* child[2];
+
+    // Since objects in a node are to be ordered in a contiguous manner,
+    // we represent them as an ordered set of object indexes
+    // [{startIdx}, {startIdx + nObjects}] instead of a list of arbitrarily
+    // ordered object indexes. The indexes refer to the elements in
+    // {AccelBVH}'s {objectList} member
+    int splitAxis, startIdx, nObjects;
 };
 
 class AccelBVH {
@@ -29,19 +41,16 @@ class AccelBVH {
 
     AccelBVH(const std::vector<std::shared_ptr<Object>>& objList,
              const int maxObjectsPerNode, const TreeSplitMethod tspMethod) :
+        // Ensure {maxObjectsPerNode} does not exceed {DEF_MAX_OBJ_PER_NODE}
         maxObjectsPerNode(std::min(DEF_MAX_OBJ_PER_NODE, maxObjectsPerNode)),
-        tspMethod(tspMethod), objectList(objList) {
-        if (objectList.size() == 0) return;
-
-        // Initialize object info list for given objects
-        std::vector<BHVObjectInfo> objectInfoList(objectList.size());
-        for (size_t i = 0; i < objectList.size(); i++)
-            objectInfoList[i] = { i, objectList[i]->worldBounds() };
-
-        // Build BVH Tree using object info list
-    }
+        tspMethod(tspMethod), objectList(objList) { constructBVH(); }
 
   private:
+    void constructBVH();
+    BVHTreeNode* constructBVHRecursive(MemoryPool& pool, std::vector<BVHObjectInfo>& objectInfoList,
+                                   int startIdx, int endIdx, int* nodeCount,
+                                   std::vector<std::shared_ptr<Object>>& orderedObjectList);
+
     const int maxObjectsPerNode;
     const TreeSplitMethod tspMethod;
     std::vector<std::shared_ptr<Object>> objectList;
--- phyray_lib/src/core/accel/bvh.cpp
+++ phyray_lib/src/core/accel/bvh.cpp
@@ -1,1 +1,44 @@
- 
+#include <core/phyr.h>
+#include <core/phyr_mem.h>
+#include <core/accel/bvh.h>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+namespace phyr {
+
+void AccelBVH::constructBVH() {
+    if (objectList.size() == 0) return;
+
+    // Initiate object info list
+    size_t sz = objectList.size();
+    std::vector<BVHObjectInfo> objectInfoList(sz);
+    for (size_t i = 0; i < sz; i++) {
+        objectInfoList[i] = { i, objectList[i]->worldBounds() };
+    }
+
+    // Create a BVH Tree from the object info list
+    int nodeCount = 0;
+    // Memory pool with a block size of 1MB
+    MemoryPool pool(1024 * 1024);
+    // Stores the ordered permutation of objectList as defined by the recursive BVH algorithm
+    std::vector<std::shared_ptr<Object>> orderedObjectList;
+
+    // Recursively build the BVH Tree
+    BVHTreeNode* root = constructBVHRecursive(pool, objectInfoList, 0, sz,
+                                          &nodeCount, orderedObjectList);
+    objectList.swap(orderedObjectList);
+}
+
+BVHTreeNode*
+AccelBVH::constructBVHRecursive(MemoryPool& pool, std::vector<BVHObjectInfo>& objectInfoList,
+                            int start, int end, int* nodeCount,
+                            std::vector<std::shared_ptr<Object>>& orderedObjectList) {
+
+    return nullptr;
+}
+
+}  // namespace phyr
+
+#pragma GCC diagnostic pop
