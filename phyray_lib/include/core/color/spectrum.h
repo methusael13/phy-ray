23d6653640b7489b47158f6d6088b7de4a6b8b04
Methusael Murmu <blendit07@gmail.com>
2017-10-19 15:40:13 +0530

    Added Spectrum conversion routines

--- phyray_lib/include/core/color/spectrum.h
+++ phyray_lib/include/core/color/spectrum.h
@@ -16,6 +16,20 @@ extern void sortSampleSpace(Real* lambda, Real* v, int n);
 extern Real averageSampleRange(const Real* lambda, const Real* v, int n,
                                Real lambda0, Real lambda1);
 
+// Conversion routines
+inline void convertXYZToRGB(const Real xyz[3], Real rgb[3]) {
+    rgb[0] =  3.240479f * xyz[0] - 1.537150f * xyz[1] - 0.498535f * xyz[2];
+    rgb[1] = -0.969256f * xyz[0] + 1.875991f * xyz[1] + 0.041556f * xyz[2];
+    rgb[2] =  0.055648f * xyz[0] - 0.204043f * xyz[1] + 1.057311f * xyz[2];
+}
+
+inline void convertRGBToXYZ(const Real rgb[3], Real xyz[3]) {
+    xyz[0] = 0.412453f * rgb[0] + 0.357580f * rgb[1] + 0.180423f * rgb[2];
+    xyz[1] = 0.212671f * rgb[0] + 0.715160f * rgb[1] + 0.072169f * rgb[2];
+    xyz[2] = 0.019334f * rgb[0] + 0.119193f * rgb[1] + 0.950227f * rgb[2];
+}
+
+
 // Spectral data declarations by CIE
 static const int nCIESamples = 471;  // 360 to 830 nm
 extern const Real CIE_X[nCIESamples];
@@ -23,7 +37,27 @@ extern const Real CIE_Y[nCIESamples];
 extern const Real CIE_Z[nCIESamples];
 extern const Real CIE_LAMBDA[nCIESamples];
 
+// Spectral data for RGB to Sampled conversion
+static const int nRGB2SpectSamples = 32;
+extern const Real RGB2SpectLambda[nRGB2SpectSamples];
+extern const Real RGBRefl2SpectWhite[nRGB2SpectSamples];
+extern const Real RGBRefl2SpectCyan[nRGB2SpectSamples];
+extern const Real RGBRefl2SpectMagenta[nRGB2SpectSamples];
+extern const Real RGBRefl2SpectYellow[nRGB2SpectSamples];
+extern const Real RGBRefl2SpectRed[nRGB2SpectSamples];
+extern const Real RGBRefl2SpectGreen[nRGB2SpectSamples];
+extern const Real RGBRefl2SpectBlue[nRGB2SpectSamples];
+extern const Real RGBIllum2SpectWhite[nRGB2SpectSamples];
+extern const Real RGBIllum2SpectCyan[nRGB2SpectSamples];
+extern const Real RGBIllum2SpectMagenta[nRGB2SpectSamples];
+extern const Real RGBIllum2SpectYellow[nRGB2SpectSamples];
+extern const Real RGBIllum2SpectRed[nRGB2SpectSamples];
+extern const Real RGBIllum2SpectGreen[nRGB2SpectSamples];
+extern const Real RGBIllum2SpectBlue[nRGB2SpectSamples];
+
 static const Real CIE_Y_LAMBDA = 106.856895;
+
+enum class SpectrumType { Reflectance, Illuminant };
 
 // CoefficientSpectrum declarations
 template <int sampleSize>
@@ -201,15 +235,74 @@ class SampledSpectrum : public CoefficientSpectrum<spectrumSampleSize> {
      * Initializes the X, Y, and Z sampled spectrum from given CIE spectrum
      */
     static void init() {
+        // Initiate X, Y, and Z spectrums
         for (int i = 0; i < nSampleSize; i++) {
             Real l0 = lerp(Real(i) / nSampleSize,
                            spectrumWavelengthStart, spectrumWavelengthEnd);
-            Real l1 = lerp(Real(i) / nSampleSize,
+            Real l1 = lerp(Real(i + 1) / nSampleSize,
                            spectrumWavelengthStart, spectrumWavelengthEnd);
             X.samples[i] = averageSampleRange(CIE_LAMBDA, CIE_X, nCIESamples, l0, l1);
             Y.samples[i] = averageSampleRange(CIE_LAMBDA, CIE_Y, nCIESamples, l0, l1);
             Z.samples[i] = averageSampleRange(CIE_LAMBDA, CIE_Z, nCIESamples, l0, l1);
         }
+
+        // Initiate spectrums for RGB to XYZ conversion
+        for (int i = 0; i < nSampleSize; ++i) {
+            Real l0 = lerp(Real(i) / nSampleSize,
+                           spectrumWavelengthStart, spectrumWavelengthEnd);
+            Real l1 = lerp(Real(i + 1) / nSampleSize,
+                           spectrumWavelengthStart, spectrumWavelengthEnd);
+            rgbRefl2SpectWhite.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBRefl2SpectWhite,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbRefl2SpectCyan.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBRefl2SpectCyan,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbRefl2SpectMagenta.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBRefl2SpectMagenta,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbRefl2SpectYellow.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBRefl2SpectYellow,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbRefl2SpectRed.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBRefl2SpectRed,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbRefl2SpectGreen.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBRefl2SpectGreen,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbRefl2SpectBlue.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBRefl2SpectBlue,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbIllum2SpectWhite.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBIllum2SpectWhite,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbIllum2SpectCyan.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBIllum2SpectCyan,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbIllum2SpectMagenta.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBIllum2SpectMagenta,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbIllum2SpectYellow.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBIllum2SpectYellow,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbIllum2SpectRed.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBIllum2SpectRed,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbIllum2SpectGreen.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBIllum2SpectGreen,
+                                       nRGB2SpectSamples, l0, l1);
+            rgbIllum2SpectBlue.samples[i] =
+                    averageSampleRange(RGB2SpectLambda, RGBIllum2SpectBlue,
+                                       nRGB2SpectSamples, l0, l1);
+        }
+    }
+
+    static SampledSpectrum getFromRGB(const Real rgb[3],
+                                      SpectrumType type = SpectrumType::Illuminant);
+    static SampledSpectrum getFromXYZ(const Real xyz[3],
+                                      SpectrumType type = SpectrumType::Reflectance) {
+        Real rgb[3]; convertXYZToRGB(xyz, rgb);
+        return getFromRGB(rgb, type);
     }
 
     /**
@@ -227,6 +320,12 @@ class SampledSpectrum : public CoefficientSpectrum<spectrumSampleSize> {
         Real scale = (spectrumWavelengthEnd - spectrumWavelengthStart) /
                      (CIE_Y_LAMBDA * nCIESamples);
         xyz[0] *= scale; xyz[1] *= scale; xyz[2] *= scale;
+    }
+
+    void toRGBConstants(Real rgb[3]) const {
+        // Calculate xyz constants and use that to convert to rgb
+        Real xyz[3]; toXYZConstants(xyz);
+        convertXYZToRGB(xyz, rgb);
     }
 
     /**
@@ -250,6 +349,16 @@ class SampledSpectrum : public CoefficientSpectrum<spectrumSampleSize> {
     static SampledSpectrum _getFromSample(const Real* lambda, const Real* v, int n);
 
     static SampledSpectrum X, Y, Z;
+    // Reflectance spectrum
+    static SampledSpectrum rgbRefl2SpectWhite, rgbRefl2SpectCyan;
+    static SampledSpectrum rgbRefl2SpectMagenta, rgbRefl2SpectYellow;
+    static SampledSpectrum rgbRefl2SpectRed, rgbRefl2SpectGreen;
+    static SampledSpectrum rgbRefl2SpectBlue;
+    // Illuminant spectrum
+    static SampledSpectrum rgbIllum2SpectWhite, rgbIllum2SpectCyan;
+    static SampledSpectrum rgbIllum2SpectMagenta, rgbIllum2SpectYellow;
+    static SampledSpectrum rgbIllum2SpectRed, rgbIllum2SpectGreen;
+    static SampledSpectrum rgbIllum2SpectBlue;
 };
 
 
