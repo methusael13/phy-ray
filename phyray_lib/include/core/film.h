30ee98a8cf0556ef5f5bb35ea5e4873e773c53d0
Methusael Murmu <blendit07@gmail.com>
2017-10-25 00:24:06 +0530

    Initial placeholder for Film class

---
+++ phyray_lib/include/core/film.h
@@ -0,0 +1,16 @@
+#ifndef PHYRAY_CORE_FILM_H
+#define PHYRAY_CORE_FILM_H
+
+#include <core/phyr.h>
+#include <core/geometry/geometry.h>
+
+namespace phyr {
+
+class Film {
+  public:
+    Point2i resolution;
+};
+
+}  // namespace phyr
+
+#endif
