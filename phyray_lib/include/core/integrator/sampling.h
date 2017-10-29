1c04122cbc12c9c19d1cb5617c0b6fb4a36bab75
Methusael Murmu <blendit07@gmail.com>
2017-10-30 03:49:02 +0530

    Added stratified sampling functions

--- phyray_lib/include/core/integrator/sampling.h
+++ phyray_lib/include/core/integrator/sampling.h
@@ -1,6 +1,7 @@
 #ifndef PHYRAY_CORE_SAMPLING_H
 #define PHYRAY_CORE_SAMPLING_H
 
+#include <core/rng.h>
 #include <core/phyr.h>
 #include <core/geometry/geometry.h>
 
@@ -9,6 +10,15 @@ namespace phyr {
 // Sampling a unit disk
 Point2f uniformSampleDisk(const Point2f& pt);
 Point2f concentricSampleDisk(const Point2f& pt);
+
+template <typename T>
+void shuffle(T* sample, int nSamples, int nDimensions, RNG& rng);
+
+// Stratified samples
+void stratifiedSample1D(Real* sample, int nSamples, bool jitter, RNG& rng);
+void stratifiedSample2D(Point2f* sample, int nxSamples, int nySamples, bool jitter, RNG& rng);
+
+void latinHypercube(Real* sample, int nSamples, int nDimensions, RNG& rng);
 
 }  // namespace phyr
 
--- phyray_lib/src/core/integrator/sampling.cpp
+++ phyray_lib/src/core/integrator/sampling.cpp
@@ -29,4 +29,53 @@ Point2f concentricSampleDisk(const Point2f& pt) {
     return Point2f(rad * std::cos(theta), rad * std::sin(theta));
 }
 
+template <typename T>
+void shuffle(T* sample, int nSamples, int nDimensions, RNG& rng) {
+    for (int i = 0; i < nSamples; i++) {
+        int swapIdx = i + rng.uniformUInt32(nSamples - i);
+        for (int j = 0; j < nDimensions; j++) {
+            std::swap(sample[nDimensions * i + j], sample[nDimensions * swapIdx + j]);
+        }
+    }
+}
+
+void stratifiedSample1D(Real* sample, int nSamples, bool jitter, RNG& rng) {
+    Real invNSamples = Real(1) / nSamples;
+    for (int i = 0; i < nSamples; i++) {
+        Real offset = jitter ? rng.uniformReal() : 0.5;
+        sample[i] = std::min((i + offset) * invNSamples, OneMinusEpsilon);
+    }
+}
+
+void stratifiedSample2D(Point2f* sample, int nxSamples, int nySamples, bool jitter, RNG& rng) {
+    Real invXSamples = Real(1) / nxSamples, invYSamples = Real(1) / nySamples;
+    for (int y = 0; y < nySamples; y++) {
+        for (int x = 0; x < nxSamples; x++) {
+            Real offX = jitter ? rng.uniformReal() : 0.5;
+            Real offY = jitter ? rng.uniformReal() : 0.5;
+            sample->x = std::min((x + offX) * invXSamples, OneMinusEpsilon);
+            sample->y = std::min((y + offY) * invYSamples, OneMinusEpsilon);
+        }
+    }
+}
+
+void latinHypercube(Real* sample, int nSamples, int nDimensions, RNG& rng) {
+    // Generate LHS samples along the diagonal
+    Real invSamples = Real(1) / nSamples;
+    for (int i = 0; i < nSamples; i++) {
+        for (int j = 0; j < nDimensions; j++) {
+            Real offset = (i + rng.uniformReal()) * invSamples;
+            sample[nDimensions * i + j] = std::min(offset, OneMinusEpsilon);
+        }
+    }
+
+    // Permute LHS samples in each dimension
+    for (int i = 0; i < nDimensions; i++) {
+        for (int j = 0; j < nSamples; j++) {
+            int swapIdx = j + rng.uniformUInt32(nSamples - j);
+            std::swap(sample[nDimensions * j + i], sample[nDimensions * swapIdx + i]);
+        }
+    }
+}
+
 }  // namespace phyr
