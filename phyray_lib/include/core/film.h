0c9815567b8d4616f0b64e2e5218e7dc7880e188
Methusael Murmu <blendit07@gmail.com>
2017-10-31 19:46:07 +0530

    Added initial Film definitions

--- phyray_lib/include/core/film.h
+++ phyray_lib/include/core/film.h
@@ -2,13 +2,49 @@
 #define PHYRAY_CORE_FILM_H
 
 #include <core/phyr.h>
+#include <core/concurrency.h>
+#include <core/integrator/filter.h>
 #include <core/geometry/geometry.h>
+
+#include <memory>
 
 namespace phyr {
 
 class Film {
   public:
-    Point2i resolution;
+    // Film size must be specified in millimetres
+    Film(const Point2i& resolution, const Bounds2f& cropWindow,
+         std::unique_ptr<Filter> filter, Real filmSize,
+         const std::string& filename, Real scale);
+
+    // Data members
+    const Point2i resolution;
+    // The diagonal length of the film in metres
+    const Real filmSize;
+    std::unique_ptr<Filter> filter;
+    // Filename for the final rendered image
+    const std::string filename;
+    // Actual image bounds in pixels
+    Bounds2i croppedImageBounds;
+
+  private:
+    struct Pixel {
+        // Represents running weighted sums of spectral pixel contributions
+        Real xyz[3] = { 0, 0, 0 };
+        // Stores the sum of filter weight values for sample contributions to pixel
+        Real filterWeightSum = 0;
+        // Stores (unweighted) sum of sample splats
+        AtomicReal splatXYZ[3];
+        // Extra padding for 32-bit structure
+        Real pad;
+    };
+
+    const Real scale;
+    std::unique_ptr<Pixel[]> pixels;
+
+    // Filter table data
+    static constexpr int filterTableSize = 16;
+    Real filterTable[filterTableSize * filterTableSize];
 };
 
 }  // namespace phyr
--- phyray_lib/src/core/film.cpp
+++ phyray_lib/src/core/film.cpp
@@ -1,1 +1,33 @@
- 
+#include <core/film.h>
+
+namespace phyr {
+
+Film::Film(const Point2i& resolution, const Bounds2f& cropWindow,
+           std::unique_ptr<Filter> filter, Real filmSize,
+           const std::string& filename, Real scale) :
+    resolution(resolution), filmSize(filmSize * .001), filter(std::move(filter)),
+    filename(filename), scale(scale) {
+    // Compute film image bounds
+    croppedImageBounds = Bounds2i(Point2i(std::ceil(resolution.x * cropWindow.pMin.x),
+                                          std::ceil(resolution.y * cropWindow.pMin.y)),
+                                  Point2i(std::ceil(resolution.x * cropWindow.pMax.x),
+                                          std::ceil(resolution.y * cropWindow.pMax.y)));
+
+    // Allocate memory for image pixels
+    pixels = std::unique_ptr<Pixel[]>(new Pixel[croppedImageBounds.area()]);
+
+    // Precompute filter weight table
+    Real invFilterTableSize = Real(1) / filterTableSize;
+    int offset = 0;
+
+    for (int y = 0; y < filterTableSize; y++) {
+        for (int x = 0; x < filterTableSize; x++) {
+            Point2f p;
+            p.x = (x + 0.5) * filter->radius.x * invFilterTableSize;
+            p.y = (y + 0.5) * filter->radius.y * invFilterTableSize;
+            filterTable[offset++] = filter->evaluate(p);
+        }
+    }
+}
+
+}  // namespace phyr
