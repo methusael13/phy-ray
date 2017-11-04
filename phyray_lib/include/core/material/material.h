b25c75fee1dcb0a70496a6d94924d0474da5f903
Methusael Murmu <blendit07@gmail.com>
2017-11-04 22:48:33 +0530

    Added Material declarations

--- phyray_lib/include/core/material/material.h
+++ phyray_lib/include/core/material/material.h
@@ -2,12 +2,26 @@
 #define PHYRAY_CORE_MATERIAL_H
 
 #include <core/phyr.h>
+#include <core/phyr_mem.h>
 
 namespace phyr {
 
-// Placeholder for Material related classes
-class TransportMode {};
+enum class TransportMode { Radiance, Importance };
 
-}
+// Material declarations
+class Material {
+  public:
+    // Interface
+
+    /**
+     * Compute the light scattering functions modelled by the object's surface
+     */
+    virtual void computeScatteringFunctions(SurfaceInteraction* si,
+                                            MemoryPool& mem, TransportMode mode,
+                                            bool allowMultiLobes) const = 0;
+    virtual ~Material();
+};
+
+}  // namespace phyr
 
 #endif
