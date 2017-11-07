6f9a1801725e12b4f169975119a946e20b601ac7
Methusael Murmu <blendit07@gmail.com>
2017-11-07 15:26:35 +0530

    Added MatteMaterial definitions

--- phyray_lib/include/modules/materials/matte.h
+++ phyray_lib/include/modules/materials/matte.h
@@ -1,15 +1,37 @@
-#ifndef PHYRAY_MODULES_MATTE_H
-#define PHYRAY_MODULES_MATTE_H
+#ifndef PHYRAY_MODULES_MATTEMAT_H
+#define PHYRAY_MODULES_MATTEMAT_H
 
+#include <memory>
+
+#include <core/color/spectrum.h>
+#include <core/material/texture.h>
 #include <core/material/material.h>
 
 namespace phyr {
 
 class MatteMaterial : public Material {
   public:
-    MatteMaterial() {}
+    MatteMaterial(const std::shared_ptr<Texture<Spectrum>>& Kd,
+                  const std::shared_ptr<Texture<Real>>& sigma) :
+        Kd(Kd), sigma(sigma) {}
+
+    // Interface
+    void computeScatteringFunctions(SurfaceInteraction* si,
+                                    MemoryPool& mem, TransportMode mode,
+                                    bool allowMultiLobes) const override;
 
   private:
+    /**
+     * @brief Kd The diffuse component
+     */
+    std::shared_ptr<Texture<Spectrum>> Kd;
+
+    /**
+     * @brief {sigma} defines the surface roughness factor.
+     * If {sigma} has a value of 0, {MatteMaterial} creates a
+     * {LambertianReflection} BRDF, otherwise the {OrenNayar} BRDF.
+     */
+    std::shared_ptr<Texture<Real>> sigma;
 };
 
 }  // namespace phyr
--- phyray_lib/src/modules/materials/matte.cpp
+++ phyray_lib/src/modules/materials/matte.cpp
@@ -1,1 +1,23 @@
- 
+#include <core/phyr_mem.h>
+#include <modules/materials/matte.h>
+#include <core/material/reflectance.h>
+
+namespace phyr {
+
+void MatteMaterial::computeScatteringFunctions(SurfaceInteraction* si,
+                                               MemoryPool& pool, TransportMode mode,
+                                               bool allowMultiLobes) const {
+    // Evaluate textures for {MatteMaterial} material and allocate BRDF
+    si->bsdf = POOL_ALLOC(pool, BSDF)(*si);
+    Spectrum r = Kd->evaluate(*si).clamp();
+    Real sig = clamp(sigma->evaluate(*si), 0, 90);
+
+    if (!r.isBlack()) {
+        if (sig == 0)
+            si->bsdf->add(POOL_ALLOC(pool, LambertianReflection)(r));
+        else
+            si->bsdf->add(POOL_ALLOC(pool, OrenNayar)(r, sig));
+    }
+}
+
+}  // namespace phyr
