c60d621d1e953b0b71612655c34f070161b5c681
Methusael Murmu <blendit07@gmail.com>
2017-11-07 06:05:15 +0530

    Initial Texture class implementation

---
+++ phyray_lib/include/core/material/texture.h
@@ -0,0 +1,19 @@
+#ifndef PHYRAY_CORE_TEXTURE_H
+#define PHYRAY_CORE_TEXTURE_H
+
+#include <core/phyr.h>
+#include <core/geometry/interaction.h>
+
+namespace phyr {
+
+template <typename T>
+class Texture {
+  public:
+    // Interface
+    virtual T evaluate(const SurfaceInteraction& si) const = 0;
+    virtual ~Texture();
+};
+
+}  // namespace phyr
+
+#endif
---
+++ phyray_lib/src/core/material/texture.cpp
@@ -0,0 +1,11 @@
+#include <core/material/texture.h>
+
+namespace phyr {
+
+/**
+ * Empty destructor. Just to create a separate vtable for this translation unit
+ */
+template <typename T>
+Texture<T>::~Texture() {}
+
+}  // namespace phyr
