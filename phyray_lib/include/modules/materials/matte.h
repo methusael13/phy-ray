c837310905613809af62238305d7d368c3d8027e
Methusael Murmu <blendit07@gmail.com>
2017-11-07 06:03:16 +0530

    Defined Matte texture

--- phyray_lib/include/modules/materials/matte.h
+++ phyray_lib/include/modules/materials/matte.h
@@ -1,1 +1,17 @@
- 
+#ifndef PHYRAY_MODULES_MATTE_H
+#define PHYRAY_MODULES_MATTE_H
+
+#include <core/material/material.h>
+
+namespace phyr {
+
+class MatteMaterial : public Material {
+  public:
+    MatteMaterial() {}
+
+  private:
+};
+
+}  // namespace phyr
+
+#endif
