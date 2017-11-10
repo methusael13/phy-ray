c0a8f0acae260bece1dc19be5085f88fd6f0ea93
Methusael Murmu <blendit07@gmail.com>
2017-11-10 06:48:23 +0530

    Modified scene description

--- phyray_app/src/main.cpp
+++ phyray_app/src/main.cpp
@@ -16,20 +16,26 @@ int main(int argc, const char* argv[]) {
     std::shared_ptr<Material> plastic(createPlasticMaterial());
 
     // Create transforms for two spheres
-    Transform transRight = Transform::translate(Vector3f(0, 0, 10));
+    Transform transRight = Transform::translate(Vector3f(3, 0, 10));
     Transform invTransRight = Transform::inverse(transRight);
-    Transform transLeft = Transform::translate(Vector3f(-5, 0, 10));
+    Transform transLeft = Transform::translate(Vector3f(-3, 0, 10));
     Transform invTransLeft = Transform::inverse(transLeft);
+    Transform lightTrans = Transform::translate(Vector3f(0, 0, 10));
+    Transform invLightTrans = Transform::inverse(lightTrans);
 
     // Create a Sphere
-    std::shared_ptr<Shape> sphere0 = createSphereShape(&transRight, &invTransRight, false, 2);
+    std::shared_ptr<Shape> sphere0 = createSphereShape(&transRight, &invTransRight, false, 1);
     std::shared_ptr<Shape> sphere1 = createSphereShape(&transLeft, &invTransLeft, false, 1);
+    std::shared_ptr<Shape> sphere2 = createSphereShape(&lightTrans, &invLightTrans, false, 0.5);
 
     // Create an Area light
-    // std::shared_ptr<AreaLight> light = createDiffuseAreaLight(transLeft, sphere1, 1, 1, 4);
+    // Real white[3] = { 1, 1, 1 };
+    // Spectrum lightSpec = Spectrum::getFromRGB(white, SpectrumType::Illuminant);
+    // std::shared_ptr<AreaLight> light =
+    //        std::make_shared<DiffuseAreaLight>(lightTrans, lightSpec, 5, sphere2);
 
     // std::shared_ptr<Light> light = createPointLight(Transform(), 30, 5, Point3f(0, 0, -2));
-    std::shared_ptr<Light> light = createDistantLight(Transform());
+    std::shared_ptr<Light> light = createDistantLight();
     // Construct a list of lights
     std::vector<std::shared_ptr<Light>> sceneLights;
     sceneLights.push_back(light);
@@ -37,11 +43,13 @@ int main(int argc, const char* argv[]) {
     // Create Geometry objects
     std::shared_ptr<Object> sphere0Obj(new GeometricObject(sphere0, metal, nullptr));
     std::shared_ptr<Object> sphere1Obj(new GeometricObject(sphere1, plastic, nullptr));
+    // std::shared_ptr<Object> lightObj(new GeometricObject(sphere2, nullptr, light));
 
     // Create a list of the scene objects
     std::vector<std::shared_ptr<Object>> sceneObjects;
     sceneObjects.push_back(sphere0Obj);
-    // sceneObjects.push_back(sphere1Obj);
+    sceneObjects.push_back(sphere1Obj);
+    // sceneObjects.push_back(lightObj);
 
     // Create the Accel structure
     std::shared_ptr<AccelBVH> accel = std::make_shared<AccelBVH>(sceneObjects, 4);
@@ -51,31 +59,18 @@ int main(int argc, const char* argv[]) {
 
     // Create film
     const Real oneOverThree = 1. / 3.;
-    std::unique_ptr<Filter> filter(new LanczosSincFilter(Vector2f(4, 4), 3));
+    std::unique_ptr<Filter> filter(new MitchellFilter(Vector2f(1, 1), Real(1)/3, Real(1)/3));
     Film* film = new Film(Point2i(480, 360),
                           Bounds2f(Point2f(0, 0), Point2f(1, 1)),
-                          std::move(filter), 35., "render01", 1);
+                          std::move(filter), 35., "render02", 50.);
 
     // Create camera
-    Transform camLook = Transform::lookAt(Point3f(0, 0, -5),
-                                          Point3f(0, 0, 1), Vector3f(0, 1, 0));
-    Bounds2f screen;
-    Real aspect = Real(film->resolution.x) / film->resolution.y;
-    if (aspect > 1) {
-        // Landscape
-        screen.pMin.x = -aspect; screen.pMax.x = aspect;
-        screen.pMin.y = -1.0f; screen.pMax.y = 1.0f;
-    } else {
-        screen.pMin.x = -1.0f; screen.pMax.x = 1.0f;
-        screen.pMin.y = -1.0f / aspect; screen.pMax.y = 1.0f / aspect;
-    }
-
-    std::shared_ptr<const Camera> camera(
-                new PerspectiveCamera(camLook, screen, 2.f, 1e6, 60., film));
+    Transform camLook = Transform::lookAt(Point3f(0, 0, 0), Point3f(0, 0, 1), Vector3f(0, 1, 0));
+    std::shared_ptr<const Camera> camera(createPerspectiveCamera(camLook, film, 0, 1e6, 45));
 
     // Create Sampler and Integrator
-    std::shared_ptr<Sampler> sampler(createStratifiedSampler(true, 8, 8));
-    Integrator* integrator = createPathIntegrator(sampler, camera, 2);
+    std::shared_ptr<Sampler> sampler(createStratifiedSampler(true, 10, 10));
+    Integrator* integrator = createPathIntegrator(sampler, camera, 5);
 
     LOG_INFO("Done constructing scene.\nRendering...");
     integrator->render(scene);
